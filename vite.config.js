7d4faeb06399f1ddfa841e994eb50a40ec3e3fae
Tushar saini <128310492+Tusharsainirox@users.noreply.github.com>
2023-11-07 05:23:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://realestate-backend-w23i.onrender.com',
         secure: false,
       },
     },
